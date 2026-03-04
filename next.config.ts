471681161acfc6f5473c563ac53b65463ab4411a
Daniel-Zo <zoudfeng@sina.com>
2026-03-04 15:26:28 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // 启用App Router
+  },
+  // 解决Vercel部署的静态资源路径问题
+  output: 'standalone',
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
